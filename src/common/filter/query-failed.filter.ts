3fc903153579710dd3249d4a64c2cd21a9e7bf48
SewonKim <sewoni37@naver.com>
2024-11-27 17:52:39 +0900

    에러 필터 적용

---
+++ src/common/filter/query-failed.filter.ts
@@ -0,0 +1,25 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+
+@Catch(QueryFailedError)
+export class QueryFailedExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost): any {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status = 400;
+
+    let message = '데이터베이스 에러 발생';
+
+    if (exception.message.includes('중복된 키')) {
+      message = 'TYPE ORM : 중복 키 에러';
+    }
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
